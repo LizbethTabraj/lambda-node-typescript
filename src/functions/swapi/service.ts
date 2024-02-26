e33d683305e83244a0ea3d0bbb1d4e3c37087998
lizbethTabraj <lizbeth.tabraj@gmail.com>
2024-02-26 00:46:54 -0500

    delete code

--- src/functions/swapi/service.ts
+++ src/functions/swapi/service.ts
@@ -8,13 +8,8 @@ import fetch from 'node-fetch';
 export const getPeople = async()=>{
 
     try {
-
-
-        const url = env.API_SWAPI;
-        
+        const url = env.API_SWAPI;        
         const body = await fetch(url+'people/').then( response => {return response.json()});
-        
-      
         return  body;
       } catch (error) {
         console.error(error);
@@ -26,16 +21,11 @@ export const getPerson = async(req)=>{
 
   try {
 
-
-    const id= req.pathParameters.id;
-    
+      const id= req.pathParameters.id;
       const url = env.API_SWAPI;
-      
       const body = await fetch(url+'people/'+id+'/').then( response => response.json());
-      
-      
-      
       return body;
+
     } catch (error) {
       console.error(error);
     }
