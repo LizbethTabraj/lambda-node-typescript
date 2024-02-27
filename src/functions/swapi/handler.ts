0a5e21561e99d44ca8005736cd792ef4e92159f5
lizbethTabraj <lizbeth.tabraj@gmail.com>
2024-02-27 15:26:34 -0500

    delete index

--- src/functions/swapi/handler.ts
+++ src/functions/swapi/handler.ts
@@ -1,36 +1,28 @@
-
-
 import { middyfy } from '@libs/lambda';
-import {getPeople, getPerson } from '@functions/swapi/service';
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import {getPeople, getPerson } from './service';
 import { addPersonCloud ,getPeopleCloud} from './conexionBD';
 
 
+const swapi = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    let result;
 
-
-
-
-const swapi = async (event) => {
- 
-
-let result;
-
-switch(event.resource){
-  case '/swapi/person/{id}': result = await getPerson(event);
-  break;
-  case '/swapi/people': result = await getPeople();
-  break;
-  case '/cloud/person': result = await addPersonCloud(event);
-  break;
-  case '/cloud/people': result = await getPeopleCloud();
-  break;
-
-}
-
-return {
-  statusCode: 200,
-  body: JSON.stringify(result),
-};
-  
+    switch(event.resource){
+      case '/swapi/person/{id}': result = await getPerson(event);
+      break;
+      case '/swapi/people': result = await getPeople();
+      break;
+      case '/cloud/person': result = await addPersonCloud(event);
+      break;
+      case '/cloud/people': result = await getPeopleCloud();
+      break;
+    
+    }
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
 };
 
 export const main = middyfy(swapi);
